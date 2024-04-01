25519845361f9457b459b3732ff3633d86a4ea49
YosrBennagra <80179447+YosrBennagra@users.noreply.github.com>
2024-04-01 03:33:48 +0100

    Update bloc.service.ts

--- src/app/_Services/bloc/bloc.service.ts
+++ src/app/_Services/bloc/bloc.service.ts
@@ -9,7 +9,7 @@ import { Bloc } from '../../_Models/bloc/bloc';
   providedIn: 'root'
 })
 export class BlocService {
-  readonly API_URL = "http://localhost:8081";
+  readonly API_URL = "http://192.168.1.196:8089";
   readonly ENDPOINT_GETALLBLOCS = "/api/bloc/all";
   readonly ENDPOINT_ADDBLOC = "/api/bloc/add";
   readonly ENDPOINT_UPDATEBLOC = "/api/bloc/update";
